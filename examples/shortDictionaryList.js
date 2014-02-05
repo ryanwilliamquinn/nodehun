21ab4c893bf0a32475f5a2e0fbf0bd428152afdf
Ryan Quinn <ryan.quinn@homes.com>
2014-02-05 13:53:58 -0500

    example showing initialization problems with multiple dictionaries

---
+++ examples/shortDictionaryList.js
@@ -0,0 +1,16 @@
+var nodehun = require('./../build/Release/nodehun');
+var fs = require('fs'),
+  config = require('./config');
+
+var affbuf,
+  dictbuf,
+  dict = {};
+
+config.statesLimit.forEach(function(state) {
+    dictionaryName = state.toLowerCase() + '_Places';
+    affbuf = fs.readFileSync(__dirname + '/dictionaries/' + dictionaryName + '/' + dictionaryName + '.aff');
+    dictbuf = fs.readFileSync(__dirname + '/dictionaries/' + dictionaryName + '/' + dictionaryName + '.dic');
+    dict[state] = new nodehun(affbuf,dictbuf);
+});
+
+dict['CA'].spellSuggest('san farncso',function(a,b){console.log('misspelling "San Francisco"',a,b)});
